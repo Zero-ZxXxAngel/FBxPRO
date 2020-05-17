b379712d1f048370b1cb55345cca3222e3f72a54
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-05-18 00:47:10 +0700

    Update FBxPRO.py

--- FBxPRO.py
+++ FBxPRO.py
@@ -132,9 +132,9 @@ CorrectPassword = "Zero"
 loop = 'true'
 while (loop == 'true'):
     username = raw_input("\033[1;96m[☆] \x1b[1;93mUsername \x1b[1;96m>>>> ")
-    if (username == UsernameSalah😘):
+    if (username == UsernameSalah):
     	password = raw_input("\033[1;96m[☆] \x1b[1;93mPassword \x1b[1;96m>>>> ")
-        if (password == PasswordSalah😘):
+        if (password == PasswordSalah):
             print "login" + username
             loop = 'false'
         else:
